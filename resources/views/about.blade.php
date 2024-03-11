d3c290f96dbc2b971131b84bb07503461d537f72
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-11 15:28:37 +0100

    added home page & about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'About')
+
+@section('main-content')
+    <h1 class="text-center mt-4">About Us</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <h1 class="text-center mt-4">My Movies</h1>
+@endsection
