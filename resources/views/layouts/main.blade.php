c3e5ed8b940273e9d5b8b5f3f01de02009530d25
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-11 15:28:07 +0100

    added layout main

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> {{ env('APP_NAME', 'Movies') }} | @yield('title')</title>
+
+    {{-- Import Vite & co --}}
+    @vite('resources/js/app.js')
+
+    {{-- CDNS --}}
+    @yield('cdns')
+
+</head>
+<body>
+
+    {{-- Header --}}
+    @include('includes.header')
+
+    {{-- Main --}}
+    <main class="container">
+        @yield('main-content')
+    </main>
+
+    {{-- Footer --}}
+    <footer></footer>
+    
+    {{-- Scripts --}}
+    @yield('scripts')
+
+</body>
+</html>
